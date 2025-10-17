5f5ab9cf91fd32d44ee24eb2b0a606ca72c7143a
Ic3 Corporation Cheats <ffroblox547@gmail.com>
2025-10-16 23:21:03 -0300

    Update execute script to load new AIM script

--- Main lib execute.lua
+++ Main lib execute.lua
@@ -1,2 +1,1 @@
-Execute this for the execute the main lib
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lk28s/GROKUILIB-IC3-CORP/refs/heads/main/GrokUILib%20universal%20AIM%20v1%20execute.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lk28s/GROKUILIB-IC3-CORP/refs/heads/main/Aim%2Besp%20univ1.lua"))()
