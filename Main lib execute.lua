3bcde7e776272f66300b8fa9b29742b9aa767713
Ic3 Corporation Cheats <ffroblox547@gmail.com>
2025-10-16 21:15:52 -0300

    Add script to execute main library from URL

---
+++ Main lib execute.lua
@@ -0,0 +1,2 @@
+Execute this for the execute the main lib
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lk28s/GROKUILIB-IC3-CORP/refs/heads/main/GrokUILib%20universal%20AIM%20v1%20execute.lua"))()
